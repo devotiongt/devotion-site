3b6b59dbd648f1321fc1051a928af7933734427e
Kevin Moran <info@devotiongt.org>
2025-08-25 20:38:32 -0600

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/devotion-site/', // Cambia esto por el nombre de tu repositorio
+  base: '/', // Para dominio personalizado
 })
